895a7b0a51b8266f9483c9fa3db9ad9abbc691f0
Kirill Alekseev <alex19892140@inbox.ru>
2025-02-22 23:15:05 +0700

    Added test for useFavoritesStore

---
+++ src/store/tests/useFavoritesStore.test.ts
@@ -0,0 +1,53 @@
+import { describe, expect, test } from "vitest"
+import { useFavoritesStore } from "../useFavoritesStore"
+import { TaskType } from "../useTasksStore"
+
+
+const mockTask: TaskType = {
+    id: 1,
+    attributes: {
+        status: 'completed',
+        title: 'Test title',
+        description: 'Test description',
+        createdAt: '2025-02-22T05:55:42.460Z',
+        updatedAt: '2025-02-22T05:55:42.460Z',
+        publishedAt: '2025-02-22T05:55:42.459Z'
+    }
+}
+
+describe('Test useFavoritesStore', () => {
+    test('test addToFavorites', () => {
+        const state = useFavoritesStore.getState()
+        state.addToFavorites(mockTask)
+        const newState = useFavoritesStore.getState()
+        expect(newState.favoritesTasks[0]).toEqual({ favorite: true, ...mockTask })
+    })
+    test('test updateFavorites', () => {
+        const updateTask: TodoType = {
+            id: 1,
+            attributes: {
+                status: 'active',
+                title: 'Test Edit title',
+                description: 'Test Edit description',
+                createdAt: '2025-02-22T05:55:42.460Z',
+                updatedAt: '2025-02-22T05:55:42.460Z',
+                publishedAt: '2025-02-22T05:55:42.459Z'
+            }
+        }
+        useFavoritesStore.setState({ favoritesTasks: [{ ...mockTask }] })
+        const state = useFavoritesStore.getState()
+        state.updateFavorites(updateTask)
+        const newState = useFavoritesStore.getState()
+        expect(newState.favoritesTasks[0].attributes.title).toBe('Test Edit title')
+        expect(newState.favoritesTasks[0].attributes.description).toBe('Test Edit description')
+        expect(newState.favoritesTasks[0].attributes.status).toBe('active')
+    })
+    test('test deleteFromFavorites', () => {
+        useFavoritesStore.setState({ favoritesTasks: [{ ...mockTask }] })
+        const state = useFavoritesStore.getState()
+        expect(state.favoritesTasks).toHaveLength(1)
+        state.deleteFromFavorites(1)
+        const newState = useFavoritesStore.getState()
+        expect(newState.favoritesTasks).toHaveLength(0)
+    })
+})
